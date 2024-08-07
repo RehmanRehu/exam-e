960a5a64fdd6e94836c100db75c5df0e07cb6142
RehmanRehu <167854050+RehmanRehu@users.noreply.github.com>
2024-08-07 16:49:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+
+      <button > Reaminder </button> &nbsp; 
+      <input type = "text" placeholder="submit" ></input> &nbsp;
+      <button > work </button> &nbsp; 
+      <input type = "text" placeholder="submit" ></input>
+    </div>
+  
+
+
+);
+}
+
+export default App;
